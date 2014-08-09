ec2a8d420f51aeb48df426dba1563a76fc1b1ff0
yongfeng <pan_yong_feng@hotmail.com>
2014-08-09 18:28:54 +0800

    showcases

--- App/Lib/Action/ShowAction.class.php
+++ App/Lib/Action/ShowAction.class.php
@@ -9,8 +9,14 @@ class ShowAction extends Action {
 
 	public function show() {
 		$m_product = M('Product');
-		echo("hi");
-		die();
+		$where = array();
+		$where['status'] = 1;
+		$order = 'hits';
+		$list = $m_product->where($where)->order($order)->select();
+		foreach($list as $k=>$v){
+
+		}
+		$this->display('showcases');
 	}
 }
 
